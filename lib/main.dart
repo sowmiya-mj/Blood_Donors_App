bc1089ea6615eb1012caa596fa5ef0a4cf6eee4e
Akshayam 360 <akshayam360@pkrarts.org>
2026-07-16 14:28:46 +0530

    2st commit

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Blood Donors App'),
+      home: const MyHomePage(title: 'Blood Donors App'),
     );
   }
 }
