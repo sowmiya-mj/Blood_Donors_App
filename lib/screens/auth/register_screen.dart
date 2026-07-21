0072ee11dd40b0fea4fb8207b0855c8bc115fe9b
Sowmiya M J <sowmiyamahalingam.mj@gmail.com>
2026-07-21 12:28:43 +0530

    12th commit

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
-import '../../widgets/location_picker.dart'; // adjust path as needed
+import '../../widgets/location_picker.dart';
 
 class RegisterScreen extends StatefulWidget {
   final String role;
